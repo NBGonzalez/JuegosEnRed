c9e17f5d4cc577e520491153cbef7ed601582fe0
AlejandroTobias <100694035+AlejandroTobias@users.noreply.github.com>
2023-11-30 13:22:18 +0100

    Funcionamiento de controles
    Controles del movimiento del coche funcionando correctamente

--- Poyecto Virtual Velocity/Code.js
+++ Poyecto Virtual Velocity/Code.js
@@ -20,10 +20,19 @@ var J1;
 var J2;
 var back;
 var gameOver = false;
-var cursorJ1;
-var cursorJ2;
+
 var tracks;
 var platforms;
+
+var teclaW;
+var teclaA;
+var teclaD;
+var teclaS;
+
+var teclaI;
+var teclaJ;
+var teclaL;
+var teclaK;
 
 var game = new Phaser.Game(config);
 
@@ -41,6 +50,11 @@ function preload ()
     this.load.image('curva4', 'assets/curva4.png');
     this.load.image('straight1', 'assets/carretera1.png');
     this.load.image('straight2', 'assets/carretera2.png');
+
+    // Coche 1
+    this.load.spritesheet('car1ver', 'assets/spritesheetvertical.png', { frameWidth: 28, frameHeight: 49 });
+    this.load.spritesheet('car1hor', 'assets/spritesheethorizontal.png', { frameWidth: 49, frameHeight: 28 });
+    this.load.spritesheet('car1dia', 'assets/spritesheetdiagonal.png', { frameWidth: 43, frameHeight: 45 });
 }
 
 function create ()
@@ -96,6 +110,68 @@ function create ()
     for(var i = 800; i<=928; i+=64){
         back.create(i, 32, 'sand');
     }
+    back.create(800, 96, 'sand');
+    back.create(800, 160, 'sand');
+
+
+    //Animaciones
+
+    this.anims.create({
+        key: 'up',
+        frames: [ { key: 'car1ver', frame: 0 } ],
+        frameRate: 20,
+        J1 : this.physics.add.sprite('car1ver')
+    });
+
+    this.anims.create({
+        key: 'down',
+        frames: [ { key: 'car1ver', frame: 1 } ],
+        frameRate: 20,
+        J1 : this.physics.add.sprite('car1ver')
+    });
+    this.anims.create({
+        key: 'right',
+        frames: [ { key: 'car1hor', frame: 0 } ],
+        frameRate: 20,
+        J1 : this.physics.add.sprite('car1hor')
+    });
+
+    this.anims.create({
+        key: 'left',
+        frames: [ { key: 'car1hor', frame: 1 } ],
+        frameRate: 20,
+        J1 : this.physics.add.sprite('car1hor')
+    });
+
+    //Anims DIAGONALES
+
+    this.anims.create({
+        key: 'rightup',
+        frames: [ { key: 'car1dia', frame: 0 } ],
+        frameRate: 20,
+        J1 : this.physics.add.sprite('car1dia')
+    });
+
+    this.anims.create({
+        key: 'rightdown',
+        frames: [ { key: 'car1dia', frame: 1 } ],
+        frameRate: 20,
+        J1 : this.physics.add.sprite('car1dia')
+    });
+
+    this.anims.create({
+        key: 'leftdown',
+        frames: [ { key: 'car1dia', frame: 2 } ],
+        frameRate: 20,
+        J1 : this.physics.add.sprite('car1dia')
+    });
+
+    this.anims.create({
+        key: 'leftup',
+        frames: [ { key: 'car1dia', frame: 3 } ],
+        frameRate: 20,
+        J1 : this.physics.add.sprite('car1dia')
+    });
     
     //platforms = this.physics.add.staticGroup();
 
@@ -103,11 +179,21 @@ function create ()
 
     //this.physics.add.image(550, 420, 'track').setScale(1.5);
 
-    J1 = this.physics.add.sprite(220, 450, 'car1').setScale(0.25).refreshBody();
+    J1 = this.physics.add.sprite(220, 450, 'car1ver');
 
     J1.setCollideWorldBounds(true);
 
-    cursors = this.input.keyboard.createCursorKeys();
+    //cursors = this.input.keyboard.createCursorKeys();
+
+    teclaW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    teclaA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    teclaD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    teclaS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+
+    teclaI = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
+    teclaJ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J);
+    teclaL = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
+    teclaK = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K);
 
     this.physics.add.collider(J1, back);
     this.physics.add.collider(J1, platforms);
@@ -115,31 +201,69 @@ function create ()
 
 function update ()
 {
-    if (cursors.left.isDown)
-        {
-            J1.setVelocityX(-100);
-        }
-        else if (cursors.right.isDown)
-        {
-            J1.setVelocityX(100);
-        }
-        else
-        {
-            J1.setVelocityX(0);
-        }
+    if(teclaA.isDown && teclaW.isDown)
+    {
+        J1.anims.play('leftup', true);
+        J1.setVelocityX(-75);
+        J1.setVelocityY(-75);
+    }
+    else if(teclaA.isDown && teclaS.isDown)
+    {
+        J1.anims.play('leftdown', true);
+        J1.setVelocityX(-75);
+        J1.setVelocityY(75);
+    }
+    else if(teclaD.isDown && teclaW.isDown)
+    {
+        J1.anims.play('rightup', true);
 
-    if (cursors.up.isDown)
-        {
-            J1.setVelocityY(-100);
-        }
-        else if (cursors.down.isDown)
-        {
-            J1.setVelocityY(100);
-        }
-        else
-        {
-            J1.setVelocityY(0);
-        }
+        J1.setVelocityX(75);
+        J1.setVelocityY(-75);
         
-    
+    }
+    else if(teclaD.isDown && teclaS.isDown)
+    {
+        J1.anims.play('rightdown', true);
+        J1.setVelocityX(75);
+        J1.setVelocityY(75);
+    }
+    else if (teclaA.isDown)
+    {
+        J1.setVelocityY(0);
+        J1.setVelocityX(-100);
+        //J1.angle = -45;
+        J1.anims.play('left', true);
+        //J1 = this.physics.add.sprite('car1hor');
+    }
+    else if (teclaD.isDown)
+    {
+        J1.setVelocityY(0);
+        J1.setVelocityX(100);
+        //J1.angle = 45;
+        J1.anims.play('right', true);
+        //J1 = this.physics.add.sprite('car1hor');
+    }
+    else if (teclaW.isDown)
+    {
+        J1.setVelocityX(0);
+        J1.setVelocityY(-100);
+        //J1.angle = 0;
+        J1.anims.play('up', true);
+        //J1 = this.physics.add.sprite('car1ver');
+    }
+    else if (teclaS.isDown)
+    {
+        J1.setVelocityX(0);
+        J1.setVelocityY(100);
+        //J1.angle = 180;
+        J1.anims.play('down', true);
+        //J1 = this.physics.add.sprite('car1ver');
+    }
+    else
+    {
+        J1.setVelocityX(0);
+        //J1.angle = 0;
+        J1.setVelocityY(0);
+        //J1.angle = 0;
+    } 
 }
