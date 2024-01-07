ff14523def6be7fc9ae06e9de931238b636cb999
PabloPrior <80918722+PabloPrior@users.noreply.github.com>
2024-01-07 21:11:09 +0100

    Código fuente final

--- Poyecto Virtual Velocity/Code.js
+++ Poyecto Virtual Velocity/Code.js
@@ -1,3 +1,4 @@
+//$(document).ready(function () {
 var config = {
     type: Phaser.AUTO,
     width: 1088,
@@ -27,6 +28,7 @@ class Jugador{
     this.numCong = 1;
     this.turb = false;
     this.numTurb = 1;
+    //this.animacionActual = null;
     }
     get velocidad(){
         return this.vel;
@@ -49,17 +51,26 @@ var estilo2 = {
 };
 
 var J1 = new Jugador(100, 75);
+J1.nombre = 'J1';
+
 var J2 = new Jugador(100, 75);
+J2.nombre = 'J2'
 
 var back;
 var gameOver = false;
 var juegoPausado = false;
+var chatActivo = false;
 var J2Win;
 
 var tracks;
 var elements;
 var detection;
 var assets;
+var icons;
+
+var icTurb;
+var icInv;
+var icCong;
 
 var teclaW;
 var teclaA;
@@ -70,6 +81,9 @@ var teclaI;
 var teclaJ;
 var teclaL;
 var teclaK;
+
+var teclaC;
+var teclaV;
 
 var teclaE;
 
@@ -97,6 +111,22 @@ var musica = document.getElementById('miMusica');
 
 var game = new Phaser.Game(config);
 
+var ip = location.host
+var connection = new WebSocket('ws://' + ip + '/echo');
+connection.onopen = function () {
+	console.log('Conexión WebSocket abierta con éxito');
+	connection.send('¡Bienvenido/a a Virtual Velocity!');
+}
+connection.onerror = function(e) {
+	console.log("WS error: " + e);
+}
+connection.onmessage = function(msg) {
+	console.log("WS message: " + msg.data);
+}
+connection.onclose = function() {
+	console.log("WS conexion cerrada");
+}
+
 function preload ()
 {
     //elememtos pista
@@ -119,6 +149,9 @@ function preload ()
     this.load.image('towell2', 'assets/toalla3.png');
     this.load.image('towell3', 'assets/toalla4.png');
     this.load.image('interface', 'assets/interfaz.png');
+    this.load.image('box', 'assets/cajaPower.png');
+    this.load.image('box2', 'assets/cajaVueltas.png');
+    this.load.image('meme', 'assets/meme.png');
 
     //imágenes de victoria
     this.load.image('ganaJ1', 'assets/ganaJ1.png');
@@ -133,6 +166,12 @@ function preload ()
     this.load.spritesheet('car2ver', 'assets/spritesheetvertical2.png', { frameWidth: 28, frameHeight: 49 });
     this.load.spritesheet('car2hor', 'assets/spritesheethorizontal2.png', { frameWidth: 49, frameHeight: 28 });
     this.load.spritesheet('car2dia', 'assets/spritesheetdiagonal2.png', { frameWidth: 43, frameHeight: 45 });
+    
+    //power-ups
+    this.load.image('icCong', 'assets/pistolacongelacion.png');
+    this.load.image('icTurb', 'assets/turbo.png');
+    this.load.image('icInv', 'assets/inversion.png');
+    
 }
 
 function create ()
@@ -165,9 +204,11 @@ function create ()
     detection = this.physics.add.staticGroup();
 
     assets = this.physics.add.staticGroup();
+    
+    icons = this.physics.add.staticGroup();
 
     detection.create(192, 512, 'meta').setScale(0.6).refreshBody();
-
+	
     //Circuito
     tracks.create(192, 256, 'straight1').setScale(0.4).refreshBody;
     tracks.create(192, 128, 'curva2').setScale(0.4).refreshBody;
@@ -196,6 +237,7 @@ function create ()
 
     //Llena la pantalla con bloques de arena
     for (let row = 32; row < 1088; row += 64) {
+		//console.log('arena');
         for (let col = 32; col < 768; col += 64) {
             let overlappingTrack = false;
 
@@ -217,14 +259,25 @@ function create ()
 
     assets.create(750, 350, 'ball');
     assets.create(350, 520, 'ball2');
-    assets.create(70, 250, 'ball3');
+    assets.create(70, 700, 'ball3');
     assets.create(835, 135, 'puddle');
     assets.create(570, 650, 'mudpuddle');
     assets.create(544, 384, 'betis');
     assets.create(830, 520, 'towell');
     assets.create(64, 490, 'towell2');
     assets.create(300, 265, 'towell3');
-    assets.create(544, 384, 'interface');
+    //assets.create(544, 384, 'interface');
+    assets.create(60, 150, 'box');
+    assets.create(60, 250, 'box');
+    assets.create(60, 350, 'box');
+    assets.create(136, 30, 'box2').setScale(3, 0.6);
+    assets.create(948, 30, 'box2').setScale(3.1, 0.6);
+    assets.create(560, 265, 'meme');
+    
+    //iconos power-up
+    icTurb = icons.create(60, 150, 'icTurb');
+	icInv = icons.create(60, 250, 'icInv');
+	icCong = icons.create(60, 350, 'icCong');
     
     //Animaciones rectas coche 1
 
@@ -371,6 +424,10 @@ function create ()
     //teclas power up turbo
     teclaQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
     teclaU = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.U);
+    
+    //tecla chat
+    teclaC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
+    teclaV = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.V);
 
     this.physics.add.collider(J1.fisicas, back);
     this.physics.add.collider(J2.fisicas, back);
@@ -381,10 +438,16 @@ function create ()
     this.physics.add.overlap(J1.fisicas, detection, cambiarJ1, null, this);
     this.physics.add.overlap(J2.fisicas, detection, cambiarJ2, null, this);
 
-    numVueltasJ1Text = this.add.text(16, 16, 'J1 Vueltas: 1/' + vueltasTotales, { fontSize: '32px', fill: '#000' });
-    numVueltasJ2Text = this.add.text(800, 16, 'J2 Vueltas: 1/' + vueltasTotales, { fontSize: '32px', fill: '#000' });
+    numVueltasJ1Text = this.add.text(24, 8, 'J1 Vueltas: 1/' + vueltasTotales, { fontSize: '32px', fill: '#000' });
+    numVueltasJ2Text = this.add.text(834, 10, 'J2 Vueltas: 1/' + vueltasTotales, { fontSize: '32px', fill: '#000' });
     numVueltasJ1Text.setStyle(estilo1);
     numVueltasJ2Text.setStyle(estilo2);
+    
+    /*this.timedEventUpdateConnection = this.time.addEvent( {
+		delay: 13,
+		callback: this.enviarPosicionesAlServidor,
+		callbackScoope: this,
+		loop: true });*/
 }
 
 function update ()
@@ -429,13 +492,30 @@ function update ()
     if(teclaU.isDown && J2.numTurb == 1){
         powerTurbo(J2);
     }
-
+    
+    
+	cargarMensajes();
     verificarFinJuego();
     document.addEventListener('keydown', function(event) {
-        if (event.key === 'Escape') {
-            pausarJuego();
-        }
+   if (event.key === 'Escape') {
+	   if(!juegoPausado && !chatActivo)
+    		pausarJuego();
+    		else if(event.key === 'Escape'){
+		if(chatActivo)
+			ocultarChat();
+	}	
+	} else if (event.key === 'c') {
+    	if (!chatActivo) {
+        	verChat();
+    	}
+	} 
+	});
+	
+	document.addEventListener('keydown', function() {
+    enviarPosicionesAlServidor(J1);
+    enviarPosicionesAlServidor(J2)
     });
+        
         }
     }
 }
@@ -466,6 +546,15 @@ function cambiarJ2(){
 
 //función de los controles con jugador, teclas y animaciones
 function controles(J, u, d, l, r,aniLU,aniLD,aniRU,aniRD,aniL,aniR,aniU,aniD){
+	 /*const mensaje = {
+        tipo: 'movimiento',
+        jugador: J.nombre,  // Agregar el nombre del jugador al mensaje
+        direccion: { izquierda: l.isDown, derecha: r.isDown, arriba: u.isDown, abajo: d.isDown },
+        posicion: { x: J.fisicas.x, y: J.fisicas.y }
+    };*/
+    //console.log('entra2');
+    //connection.send(JSON.stringify(mensaje));
+	
     if(l.isDown && u.isDown)
     {
         J.fisicas.anims.play(aniLU, true);
@@ -530,6 +619,7 @@ function controles(J, u, d, l, r,aniLU,aniLD,aniRU,aniRD,aniL,aniR,aniU,aniD){
         J.fisicas.setVelocityY(0);
         //J1.angle = 0;
     } 
+    
 }
 
 function verificarFinJuego() {
@@ -553,6 +643,19 @@ function reanudarJuego() {
     juegoPausado = false;
 }
 
+function verChat() {  
+    document.getElementById('chat-container').style.display = 'block';
+    document.getElementById('chat2').style.display = 'block';
+    chatActivo = true;
+    //juegoPausado = true;
+}
+
+function ocultarChat() { 
+    document.getElementById('chat2').style.display = 'none';
+    chatActivo = false;
+    //juegoPausado = true;
+}
+
 function sonidoPower(ruta) {
     //elemento de audio
     var audio = new Audio(ruta);
@@ -565,6 +668,19 @@ function powerInversion(J, usu) {
     sonidoPower('assets/invertir.mp3');
     J.inver = true;
     usu.numInver = 0;
+    icInv.setVisible(false);
+    // Construir el mensaje de power-up
+    //const mensajePowerUp = {
+       // tipo: 'inversion',
+        //jugador: miID,  // Asegúrate de tener un identificador único para el jugador
+   // };
+
+    // Enviar el mensaje al servidor a través del WebSocket
+    //socket.send(JSON.stringify(mensajePowerUp));
+    document.getElementById('mensajePelota').style.display = 'block';
+	 setTimeout(function () {
+            document.getElementById('mensajePelota').style.display = 'none';
+        }, 3000);
     setTimeout(function() {
         noPowerInversion(J);
     }, 5000);
@@ -579,6 +695,7 @@ function powerCongelacion(J, usu){
     J.vel = 0;
     J.velD = 0;
     usu.numCong = 0;
+    icCong.setVisible(false);
     setTimeout(function() {
         noPowerCongelacion(J);
     }, 5000);
@@ -594,6 +711,7 @@ function powerTurbo(J){
     J.vel *= 1.5;
     J.velD *= 1.5;
     J.numTurb = 0;
+    icTurb.setVisible(false);
     setTimeout(function() {
         noPowerTurbo(J);
     }, 5000);
@@ -613,8 +731,12 @@ function finalizarPartida() {
     back.clear(true, true);      
     detection.clear(true, true);
     assets.clear(true, true);
+    icons.clear(true, true);
     numVueltasJ1Text.setVisible(false);
     numVueltasJ2Text.setVisible(false);
+    ocultarChat();
+    reanudarJuego();
+    document.getElementById('chat-container').style.display = 'none';
     sonidoPower('assets/final.mp3')
     if(numVueltasJ1 > vueltasTotales){
         J2Win.setVisible(false);
@@ -625,3 +747,99 @@ function finalizarPartida() {
         window.location.href = 'Index.html';
     }, 5000); 
 }
+
+function enviarPosicionesAlServidor(J) {
+    const mensaje = {
+        tipo: 'posicion',
+        jugador: J.nombre,
+        posicion: { x: J.fisicas.x, y: J.fisicas.y },
+        velocidad: { velX: J.fisicas.body.velocity.x, velY: J.fisicas.body.velocity.y },
+        animacion: { currentAnim: J.fisicas.anims.currentAnim }
+        //animacion: {anim: J.fisicas.anims}
+    };
+    
+    connection.send(JSON.stringify(mensaje));
+}
+
+connection.onmessage = function (msg) {
+    var mensaje = JSON.parse(msg.data);
+
+    switch (mensaje.tipo) {
+        case 'posicion':
+            actualizarPosicionJugador(mensaje.jugador, mensaje.posicion, mensaje.velocidad, mensaje.animacion);
+            //controles(mensaje.jugador, teclaW, teclaS, teclaA, teclaD, mensaje.animacion,mensaje.animacion,mensaje.animacion,mensaje.animacion,mensaje.animacion,mensaje.animacion,mensaje.animacion,mensaje.animacion)
+            break;
+        // otros casos...
+    }
+}
+
+function actualizarPosicionJugador(nombreJugador, posicion, velocidad, animacion) {
+    if (nombreJugador === 'J1' && J1.fisicas) {
+        J1.fisicas.setPosition(posicion.x, posicion.y);
+        J1.fisicas.setVelocity(velocidad.velX, velocidad.velY);  // Ajustamos aquí para establecer ambas velocidades
+        J1.fisicas.anims.play(animacion.currentAnim || 'up', true); // Si no hay animación, reproducir una predeterminada
+    } else if (nombreJugador === 'J2' && J2.fisicas) {
+        J2.fisicas.setPosition(posicion.x, posicion.y);
+        J2.fisicas.setVelocity(velocidad.velX, velocidad.velY);
+        J2.fisicas.anims.play(animacion.currentAnim || 'up2', true);
+    }
+}
+
+
+document.addEventListener('keydown', function (event) {
+    // Detener la propagación del evento si el chat está activo
+    if (chatActivo) {
+        event.stopPropagation();
+    }
+});
+
+/*connection.onmessage = function (msg) {
+    var data = JSON.parse(msg.data);
+    if (data.tipo === 'movimiento') {
+        // Actualizar el movimiento del coche en el juego
+        actualizarMovimiento(data);
+    } else {
+        // Otro tipo de mensajes si es necesario
+    }
+};
+
+function actualizarMovimiento(data) {
+    // Obtener la dirección del movimiento del mensaje
+    const direccion = data.direccion;
+
+    // Obtener el jugador del mensaje
+    const jugador = data.jugador;
+
+    // Obtener el objeto del jugador correspondiente
+    let jugadorObj;
+    if (jugador === 'J1') {
+        jugadorObj = J1;
+    } else if (jugador === 'J2') {
+        jugadorObj = J2;
+    } else {
+        jugadorObj = J1;
+    }
+
+    // Actualizar la posición del jugador en el juego según la dirección del mensaje
+    if (direccion.izquierda && direccion.arriba) {
+        controles(jugadorObj, teclaW, teclaS, teclaA, teclaD,'leftup','leftdown','rightup','rightdown','left','right','up','down');
+    } else if (direccion.izquierda && direccion.abajo) {
+        controles(jugadorObj, teclaW, teclaS, teclaA, teclaD,'leftup','leftdown','rightup','rightdown','left','right','up','down');
+    } else if (direccion.derecha && direccion.arriba) {
+        controles(jugadorObj, teclaW, teclaS, teclaA, teclaD,'leftup','leftdown','rightup','rightdown','left','right','up','down');
+    } else if (direccion.derecha && direccion.abajo) {
+        controles(jugadorObj, teclaW, teclaS, teclaA, teclaD,'leftup','leftdown','rightup','rightdown','left','right','up','down');
+    } else if (direccion.izquierda) {
+        controles(jugadorObj, teclaW, teclaS, teclaA, teclaD,'leftup','leftdown','rightup','rightdown','left','right','up','down');
+    } else if (direccion.derecha) {
+        controles(jugadorObj, teclaW, teclaS, teclaA, teclaD,'leftup','leftdown','rightup','rightdown','left','right','up','down');
+    } else if (direccion.arriba) {
+        controles(jugadorObj, teclaW, teclaS, teclaA, teclaD,'leftup','leftdown','rightup','rightdown','left','right','up','down');
+    } else if (direccion.abajo) {
+        controles(jugadorObj, teclaW, teclaS, teclaA, teclaD,'leftup','leftdown','rightup','rightdown','left','right','up','down');
+    } else {
+        // Si no hay ninguna dirección, detener el movimiento del jugador
+        jugadorObj.fisicas.setVelocity(0, 0);
+    }
+}*/
+//});
